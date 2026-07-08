16a67533d9e6029f5d23566a4036e596c47020a5
michael-marras <marrasmichael2499@gmail.com>
2026-07-07 23:28:43 -0700

    using this as a checkpoint for finetuning whisper

--- lib/Head/Head.cpp
+++ lib/Head/Head.cpp
@@ -5,7 +5,7 @@ constexpr uint16_t SIXTEEN_KHZ      = 16000;
 constexpr uint8_t  PDM_MIC_DATA_PIN = 41;
 constexpr uint8_t  PDM_MIC_CLK_PIN  = 42;
 constexpr uint32_t TWENTY_MHZ       = 20000000;
-constexpr uint16_t PORT             = 9998;
+constexpr uint16_t PORT             = 9997;
 constexpr uint8_t  CPU_CORE         = 1;
 
 constexpr uint8_t  MIC_TASK_PRIORITY = 4;
@@ -254,7 +254,7 @@ void Head::microphoneTaskEntry(void* pvParameters) {
 }
 
 void Head::receiveCommandsTaskEntry(void* pvParameters) {
-	static_cast<Head*>(pvParameters) -> microphoneTask(); 
+	static_cast<Head*>(pvParameters) -> receiveCommandsTask(); 
 }
 
 void Head::cameraTask() {
--- src/local-server/server.py
+++ src/local-server/server.py
@@ -3,27 +3,34 @@ import wave
 import threading
 import shutil
 import numpy as np 
+import queue
+
 from pywhispercpp.model import Model
 
-PORT               = 9998
+PORT               = 9997
 HEADER             = 64
 WAV_PATH           = "./out/audio/test.wav"
 VAD_PATH           = "./models/ggml-silero-v6.2.0.bin"
 TXT_PATH           = "./out/audio/test.txt"
-NUM_FRAMES         = 16000
+NUM_FRAMES         = 40000
 NUM_CHANNELS       = 1
 SAMPLE_WIDTH_BYTES = 2
 WAV_FRAME_RATE_HZ  = 16000
+TARGET_BYTES       = 320000
+GAIN               = 3
+
+def openWavFile(file_path, mode):
+    wavFile = wave.open(file_path, mode)
+    wavFile.setnchannels(NUM_CHANNELS)
+    wavFile.setsampwidth(SAMPLE_WIDTH_BYTES)
+    wavFile.setframerate(WAV_FRAME_RATE_HZ)
+    return wavFile
 
 def initAudioServer():
     server1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     server1.bind(('0.0.0.0', PORT))
 
-    wavFile = wave.open(WAV_PATH, 'wb')
-    wavFile.setnchannels(NUM_CHANNELS)
-    wavFile.setsampwidth(SAMPLE_WIDTH_BYTES)
-    wavFile.setframerate(WAV_FRAME_RATE_HZ)
-
+    wavFile = openWavFile(WAV_PATH, "wb")
     textFile = open(TXT_PATH, "w")
 
     return server1, wavFile, textFile
@@ -34,42 +41,49 @@ def initVideoServer():
     server2.listen(1)
 
     mjpegFile = open("./out/video/test.mjpeg", "wb")
-
     client, addr = server2.accept()
+    
     return client, mjpegFile
 
-def runInference(model, buffer, textFile):
-    numpy_array = np.frombuffer(buffer, dtype = np.int16)
+def conditionAudio(audio):
+    numpy_array = np.frombuffer(audio, dtype = np.int16)
     array_normalized = numpy_array.astype(np.float32) / 32768.0
-    segments = model.transcribe(array_normalized)
-    for segment in segments:
-        print(segment.text)
 
-def runAudioServer(socketUDP, wavFile, model, textFile):
+    return GAIN * array_normalized
+
+def transcribe(model, queue):
+    # Condition the audio from queue
+    while True:
+        array_normalized = conditionAudio(queue.get())
+
+        # Transcribe and print
+        segments = model.transcribe(array_normalized)
+        for segment in segments:
+            print(segment.text)
+
+def runAudioServer(socketUDP, wavFile, textFile, audioQ):
     buffer = bytearray()
-    thread3 = None
+
     while True:
         data, addr = socketUDP.recvfrom(512)
         buffer.extend(data)
         wavFile.writeframes(data)
+
         if wavFile.tell() >= NUM_FRAMES:
-            if thread3 is not None and thread3.is_alive():
-                thread3.join()   
             buffer_copy = buffer.copy()
+            audioQ.put(buffer_copy)
             buffer.clear()
-            thread3 = threading.Thread(target = runInference, args = (model, buffer_copy, textFile))
-            thread3.start()
             wavFile.close()
             shutil.copy(WAV_PATH, "chunk.wav")
-            wavFile = wave.open(WAV_PATH, 'wb')
-            wavFile.setnchannels(NUM_CHANNELS)
-            wavFile.setsampwidth(SAMPLE_WIDTH_BYTES)
-            wavFile.setframerate(WAV_FRAME_RATE_HZ)
+            wavFile = openWavFile(WAV_PATH, "wb")
             
 def runVideoServer(socketTCP, file):
     while True:
         videoData = socketTCP.recv(1024)
         file.write(videoData)
+
+# Entry Point
+audioQ = queue.Queue()
 
 print("Server Initializing")
 
@@ -77,14 +91,18 @@ socketUDP, wavFile, textFile = initAudioServer()
 socketTCP, mjpegFile = initVideoServer()
 
 model = Model(
-    model = 'base.en', 
+    model = 'small.en', 
     print_realtime=False, 
-    print_progress=False
+    print_progress=False,
+    audio_ctx=192,
+    suppress_blank=True,
+    single_segment=True
 )
 
-thread1 = threading.Thread(target = runAudioServer, args = (socketUDP, wavFile, model, textFile))
+thread1 = threading.Thread(target = runAudioServer, args = (socketUDP, wavFile, textFile, audioQ))
 thread2 = threading.Thread(target = runVideoServer, args = (socketTCP, mjpegFile))
+thread3 = threading.Thread(target = transcribe, args = (model, audioQ))
 
 thread1.start()
 thread2.start()
-    
+thread3.start()
